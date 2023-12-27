2acafbbbfb6706d4eaf118f74e385b641d988b55
mikecebul <mikecebul@gmail.com>
2023-12-27 01:40:40 -0500

    Chore: add tailwind, shadcn-ui

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,12 @@
 import { createRoot } from "react-dom/client";
+import { Button } from "./components/ui/button";
 
 const root = createRoot(document.body);
-root.render(<h2>Hello from React!!</h2>);
+root.render(
+  <main className="flex flex-col justify-center items-center h-[100svh]">
+    <h2 className="text-primary text-3xl pb-16">
+      Let's generate some progress reports.
+    </h2>
+    <Button>Generate Reports</Button>
+  </main>
+);
--- webpack.renderer.config.ts
+++ webpack.renderer.config.ts
@@ -1,11 +1,19 @@
-import type { Configuration } from 'webpack';
-
-import { rules } from './webpack.rules';
-import { plugins } from './webpack.plugins';
+import type { Configuration } from "webpack";
+import path from "path"
+import { rules } from "./webpack.rules";
+import { plugins } from "./webpack.plugins";
 
 rules.push({
   test: /\.css$/,
-  use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
+  use: [
+    { loader: "style-loader" },
+    {
+      loader: "css-loader",
+    },
+    {
+      loader: "postcss-loader",
+    },
+  ],
 });
 
 export const rendererConfig: Configuration = {
@@ -14,6 +22,9 @@ export const rendererConfig: Configuration = {
   },
   plugins,
   resolve: {
-    extensions: ['.js', '.ts', '.jsx', '.tsx', '.css'],
+    extensions: [".js", ".ts", ".jsx", ".tsx", ".css"],
+    alias: {
+      '@': path.resolve(__dirname, 'src/'), // Adjust the path as necessary
+    },
   },
 };
